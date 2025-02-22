d4794b91846e8ec93e2a62b4948420e989c34a70
Vasilii Laktyushin <laktyushin_va@nlmk.com>
2025-02-22 17:35:56 +0300

    Add main structure of project

---
+++ application/task-service/src/test/java/ru/srfholding/TaskServiceApplicationTest.java
@@ -0,0 +1,13 @@
+package ru.srfholding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskServiceApplicationTest {
+
+    @Test
+    void contextLoads() {
+
+    }
+}
---
+++ library/tracker-models/src/test/java/ru/srfholding/trackermodels/TrackerModelsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.srfholding.trackermodels;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackerModelsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
