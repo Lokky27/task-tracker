dc1f2a3d695697ffc9a5f1d5e82a22d70288c1a1
Лактюшин Василий Андреевич <laktyushin_va@nlmk.com>
2025-08-08 22:20:12 +0300

    Refactor modules + add migrations for all services

--- library/tracker-models/src/test/resources/sql/init_schema.sql
+++ library/tracker-models/src/test/resources/sql/init_schema.sql
@@ -1,1 +1,1 @@
-CREATE SCHEMA IF NOT EXISTS task_tracker;
+CREATE SCHEMA IF NOT EXISTS user_details_service_schema;
---
+++ scripts/init.sql
@@ -0,0 +1,17 @@
+DO
+$$
+    BEGIN
+        -- Создание схем для каждого микросервиса
+        CREATE SCHEMA IF NOT EXISTS user_details_service_schema; -- Для user-service
+        CREATE SCHEMA IF NOT EXISTS team_service_schema; -- Для team-service
+        CREATE SCHEMA IF NOT EXISTS project_service_schema; -- Для project-service
+        CREATE SCHEMA IF NOT EXISTS task_service_schema; -- Для task-service
+        CREATE SCHEMA IF NOT EXISTS notification_service_schema; -- Для notification-service
+        CREATE SCHEMA IF NOT EXISTS activity_service_schema; -- Для activity-service
+        CREATE SCHEMA IF NOT EXISTS auth_service_schema; -- Для auth-service
+        CREATE SCHEMA IF NOT EXISTS file_service_schema; -- Для file-service
+        CREATE SCHEMA IF NOT EXISTS comment_service_schema; -- Для comment-service
+        CREATE SCHEMA IF NOT EXISTS reporting_schema; -- Для reporting-service
+        CREATE SCHEMA IF NOT EXISTS search_service_schema; -- Для search-service
+    END
+$$;
